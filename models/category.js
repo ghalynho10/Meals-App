97d1eaa66d97c8f0e2e23d1d957499138e3276b5
ghalynho10 <mghalynho@gmail.com>
2020-06-12 19:37:25 -0400

    Adding a grid view

---
+++ models/category.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class category {
+    constructor(id, title, color) {
+        this.id = id
+        this.title = title
+        this.color = color
+    }
+}
+
+export default category
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
     return (
-        <View style={styles.screen}>
-            <Text>The categories Screen!</Text>
-            <Button title="Go to Meals" onPress={() => {
-                props.navigation.navigate({ routeName: 'CategoryMeals' })
-            }} />
-        </View>
-    )
-}
+      <View style={styles.gridItem}>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
 
-export default CategoriesScreen
+export default CategoriesScreen;
