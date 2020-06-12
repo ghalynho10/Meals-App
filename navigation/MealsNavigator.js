688740bca6d5bd62d8b3e3f280c1d2f4d9f0e9cb
ghalynho10 <mghalynho@gmail.com>
2020-06-12 16:07:19 -0400

    Adding react navigation

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createStackNavigator } from 'react-navigation-stack'
+import CategoriesScreen from '../screens/CategoriesScreen'
+import CategoryMealsScreen from '../screens/CategoryMealsScreen'
+import MealDetailScreen from '../screens/MealDetailScreen'
+import { createAppContainer } from 'react-navigation'
+
+const MealsNavigator = createStackNavigator({
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailScreen
+})
+
+
+
+export default createAppContainer(MealsNavigator)
