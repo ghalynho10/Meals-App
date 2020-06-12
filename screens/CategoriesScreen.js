91135afdf333955729cd6723a595476bbbc960c2
ghalynho10 <mghalynho@gmail.com>
2020-06-11 20:11:39 -0400

    created screens components

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const CategoriesScreen = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default CategoriesScreen
