b437c8220f12fc3dcf85de3bb2a49a130b1f04dc
ghalynho10 <mghalynho@gmail.com>
2020-06-20 14:20:04 -0400

    connecting the different screens to redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import MealList from "../components/MealList";
-import { MEALS } from "../data/dummy-data";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
 
 const FavoritesScreen = (props) => {
-  const favMeal = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
+  const favMeal = useSelector((state) => state.meals.favoriteMeals);
+
   return <MealList listData={favMeal} navigation={props.navigation} />;
 };
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,9 @@
-import React from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  Button,
-  ScrollView,
-  Image,
-} from "react-native";
+import React, { useEffect } from "react";
+import { View, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { MEALS } from "../data/dummy-data";
 import HeaderButton from "../components/HeaderButton";
 import DefaultText from "../components/DefaultText";
+import { useSelector } from "react-redux";
 
 const LisItem = (props) => {
   return (
@@ -22,7 +15,13 @@ const LisItem = (props) => {
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const availableMeals = useSelector((state) => state.meals.meals);
+  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setParams({ mealTitle: selectedMeal.title });
+  }, [selectedMeal]);
+
   return (
     <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -45,10 +44,10 @@ const MealDetailScreen = (props) => {
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam("mealTitle");
 
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
